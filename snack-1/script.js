b0f574299965afeaf36cebace125f90b78d1a748
alessandro-sartini <ale.sartini.98@gmail.com>
2025-01-27 12:16:01 +0100

    ex 3 ok

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,7 +3,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-names.forEach((el)=> {
+names.forEach((el) => {
+    
     console.log(el)
 
 })
